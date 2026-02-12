dd923b73c3d703e30c984c7c2749b47ef9af8218
John Steel <hi.johnstill0@gmail.com>
2026-02-12 07:48:19 +0200

    remove file based loaders

--- src/QuickBotz.ts
+++ src/QuickBotz.ts
@@ -14,31 +14,26 @@ class QuickBotz {
         this.commands = new Collection();
     };
 
-    useCommandsFolder(pathStr: string) {
-        const foldersPath = path.join(process.cwd(), pathStr);
-        const commandFolders = fs.readdirSync(foldersPath);
-        for (const folder of commandFolders) {
-            const commandsPath = path.join(foldersPath, folder);
-            const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js') || file.endsWith('.ts'));
+    // async useCommandsFolder(pathStr: string) {
+    //     const foldersPath = path.join(process.cwd(), pathStr);
+    //     const commandFolders = fs.readdirSync(foldersPath);
+    //     for (const folder of commandFolders) {
+    //         const commandsPath = path.join(foldersPath, folder);
+    //         const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js') || file.endsWith('.ts'));
 
-            for (const file of commandFiles) {
-                const filePath = path.join(commandsPath, file);
-                const command = require(filePath);
-                if ('data' in command && 'execute' in command) {
-                    this.commands.set(command.data.name, command);
-                } else {
-                    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-                }
-            }
-        }
-        return this.commands.size;
-    }
+    //         for (const file of commandFiles) {
+    //             const filePath = path.join(commandsPath, file);
+    //             const command = (await import(filePath)).default;
+    //             console.log(command)
+    //         }
+    //     }
+    // }
 
-    useEventsFolder(path: string) {
+    // useEventsFolder(path: string) {
 
-    }
+    // }
 
-    login = () => this.client.login(this.token)
+    start = () => this.client.login(this.token)
 };
 
 export default QuickBotz;
